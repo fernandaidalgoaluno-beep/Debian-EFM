d85f7d1ed4f4118fdb101c70eb5f9bc599ca3b63
math <matheusslimatndd7@gmail.com>
2026-09-23 16:12:43 -0300

    Create media.c
    Código em C realizado no Debian

---
+++ C/media.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  float nota1, nota2, media;
+
+  printf("Digite a primeira nota: ");
+  scanf("%f", &nota1);
+
+  printf("Digite a segunda nota: ");
+  scanf("%f", &nota2);
+
+  media = (nota1 + nota2) / 2;
+
+  printf("\nMedia: %2f\n", media);
+
+  if(media >= 6.0) {
+        printf("Aprovado\n");
+  } else {
+        printf("Reprovado\n");
+  }
+
+  return 0;
